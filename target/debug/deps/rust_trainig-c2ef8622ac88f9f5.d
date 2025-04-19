8fcd064e6091e0489df0a1cefac3208e8142bdcc
Haim Sarraf <138432854+HaimSarraf@users.noreply.github.com>
2025-04-19 11:02:04 +0330

    step-1

---
+++ target/debug/deps/rust_trainig-c2ef8622ac88f9f5.d
@@ -0,0 +1,5 @@
+/home/haim/Desktop/rust-training/rust-trainig/target/debug/deps/librust_trainig-c2ef8622ac88f9f5.rmeta: src/main.rs
+
+/home/haim/Desktop/rust-training/rust-trainig/target/debug/deps/rust_trainig-c2ef8622ac88f9f5.d: src/main.rs
+
+src/main.rs:
