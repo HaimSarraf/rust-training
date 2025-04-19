802d88f8d9c19bf458bfcebb9b22d78f9e543c5b
Haim Sarraf <sarhangaurelianobuendia@yahoo.com>
2025-04-19 11:25:41 +0330

    step-3

--- rust-trainig/src/main.rs
+++ rust-trainig/src/main.rs
@@ -1,4 +1,13 @@
 fn main() {
-    let x =5;
-    print!("The value of x = {}", x);
+    let mut x =5;
+ 
+    print!("The Value of x = {}\n", x);
+ 
+    x=10;
+
+    print!("The Value of x = {x}\n");
+
+    const HOUR_IN_SECONDS : u32 = 60 * 60;
+
+    print!("1 Hour = {} Seconds\n" , HOUR_IN_SECONDS);
 }
---
+++ rust-trainig/target/debug/deps/rust_trainig-d858a622109e6f17.d
@@ -0,0 +1,5 @@
+/home/haim/Desktop/rust-training/rust-trainig/target/debug/deps/rust_trainig-d858a622109e6f17: src/main.rs
+
+/home/haim/Desktop/rust-training/rust-trainig/target/debug/deps/rust_trainig-d858a622109e6f17.d: src/main.rs
+
+src/main.rs:
