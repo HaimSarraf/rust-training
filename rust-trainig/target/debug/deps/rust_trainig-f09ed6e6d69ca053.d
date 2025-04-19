66674e17b32abf372e299348bf7cbb61b2b508f1
Haim Sarraf <sarhangaurelianobuendia@yahoo.com>
2025-04-19 10:59:45 +0330

    step-1

---
+++ rust-trainig/target/debug/deps/rust_trainig-f09ed6e6d69ca053.d
@@ -0,0 +1,5 @@
+/home/haim/Desktop/rust-training/rust-trainig/target/debug/deps/librust_trainig-f09ed6e6d69ca053.rmeta: src/main.rs
+
+/home/haim/Desktop/rust-training/rust-trainig/target/debug/deps/rust_trainig-f09ed6e6d69ca053.d: src/main.rs
+
+src/main.rs:
