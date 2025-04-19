5171294c6c0852be94fca1e30d0769ae148d6ced
Haim Sarraf <sarhangaurelianobuendia@yahoo.com>
2025-04-19 11:04:32 +0330

    variables

---
+++ rust-trainig/target/debug/deps/rust_trainig-e02cfb782c41e1aa.d
@@ -0,0 +1,5 @@
+/home/haim/Desktop/rust-training/rust-trainig/target/debug/deps/rust_trainig-e02cfb782c41e1aa: src/main.rs
+
+/home/haim/Desktop/rust-training/rust-trainig/target/debug/deps/rust_trainig-e02cfb782c41e1aa.d: src/main.rs
+
+src/main.rs:
---
+++ rust-trainig/target/debug/rust-trainig.d
@@ -0,0 +1,1 @@
+/home/haim/Desktop/rust-training/rust-trainig/target/debug/rust-trainig: /home/haim/Desktop/rust-training/rust-trainig/src/main.rs
