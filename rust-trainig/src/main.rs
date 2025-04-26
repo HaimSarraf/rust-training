cd90eab64f28e3e6fd7091029c22f20d12d19b47
Haim Sarraf <sarhangaurelianobuendia@yahoo.com>
2025-04-26 12:36:52 +0330

    fibo

--- rust-trainig/src/main.rs
+++ rust-trainig/src/main.rs
@@ -3,22 +3,22 @@ use std::io;
 fn main() {
     println!("Enter a number:");
 
-    let mut n: i32;        //the input which user enters
+    let mut _n: i32;        //the input which user enters
     let mut fibo: i32 = 1; //the n'th sentence of fibonacci
     let mut a:i32 = 1;     //counter one
-    let mut b:i32 = 1;     //counter two
+    let mut b:i32 = 1; //counter two
 
     let mut input: String = String::new();
     io::stdin()
         .read_line(&mut input)
         .expect("Failed to read line");
-    n = input.trim().parse().expect("Please enter a number");
+    _n = input.trim().parse().expect("Please enter a number");
 
 
-    if n == 1 || n == 2 {
+    if _n == 1 || _n == 2 {
         println!("{}", fibo);
     } else {
-        for _ in 3..=n {
+        for _ in 3..=_n {
             fibo = a + b;
             a = b;
             b = fibo;
