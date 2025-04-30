fb95a8bd11db4792b938244fb02bfe6479458577
Haim Sarraf <sarhangaurelianobuendia@yahoo.com>
2025-04-30 10:38:44 +0330

    scope-variables

--- rust-trainig/src/main.rs
+++ rust-trainig/src/main.rs
@@ -5,22 +5,37 @@ fn main() {
     {
         //* "s" is  valid just in this scope
         //* after we do some stuff with it and 
-        //* exit the scope, it would be no longer valid
+        //* exit the scope, it would be no longer valid.
     
 
         let mut s1: String = String::from("hello");
 
+        println!("s1 before assignment & push_str: {s1}");
+        
+        s1 = String::from("yoohoo");
+        //* when assign a new value to an
+        //* existing variable, Rust will free
+        //* the original value's memory immediately.
+        
         s1.push_str(" world !");
 
-        let s2 = s1;
+
+        let s2 = s1.clone();
+
+        println!("s1 still exists : {s1}");
         
+        let s3 = s1;
+
+        println!("after 'drop' s1 doesn't last anymore !");
+
         // println!("{s1}"); 
         //* to prevent 'double free' error,
-        //* Rust consider s1 is no longer valid
+        //* Rust consider s1 is no longer valid.
         
-        println!("{s2}");
+        println!("s2 (the deep copy of s1 by .clone() method):{s2}");
+        println!("s3 (just moved s1 in it):{s3}");
 
-    } //* after this curly bracket,Rust calls
-      //* the \drop\ function
+    } //* after this curly bracket,Rust calls the \drop\ function.
 
+    
 }
